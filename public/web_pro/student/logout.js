bc94a30162484227a17717c8eda7c1abb7fd4560
Norra97 <norraphat1196@gmail.com>
2025-06-20 17:57:27 +0700

    แยก front back แล้ว

--- public/web_pro/student/logout.js
+++ public/web_pro/student/logout.js
@@ -2,3 +2,8 @@ function logout() {
     localStorage.clear(); // ล้างข้อมูลใน localStorage
     window.location.href = "/"; // เปลี่ยนเส้นทางไปยังหน้า "/"
 }
+
+function formatDate(dateString) {
+    if (!dateString) return 'N/A';
+    return dateString.split('T')[0];
+}
